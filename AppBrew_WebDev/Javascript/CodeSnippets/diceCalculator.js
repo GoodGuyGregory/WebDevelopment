0d1e267066d7a29cf40d906bcecf37cc4be381b2
GoodGuyGregory <greg.witt625@gmail.com>
2019-03-18 08:16:51 -0400

    completes randomnumber exercises

---
+++ AppBrew_WebDev/Javascript/CodeSnippets/diceCalculator.js
@@ -0,0 +1,8 @@
+var n = Math.random();
+//increases the range of numbers
+n *= 6;
+//Rounds the Numbers from 0-5 and adds 1
+// increasing the range to 6
+n = Math.floor(n) + 1;
+// Displays the number
+console.log(n);
---
+++ AppBrew_WebDev/Javascript/CodeSnippets/loveCalculator.js
@@ -0,0 +1,7 @@
+var lover1 = prompt("Enter Your Names for Love: ");
+var lover2 = prompt("Enter Your Names for Love: ");
+
+var loveScore = Math.random();
+loveScore *= 100;
+loveScore = Math.floor(loveScore) + 1;
+alert("Your Love Score is : " + loveScore + "%");
