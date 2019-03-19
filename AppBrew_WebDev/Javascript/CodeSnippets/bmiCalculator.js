c83ba0ad8f6f149176dd77b1b11ab511dbcd0c32
GoodGuyGregory <greg.witt625@gmail.com>
2019-03-19 19:05:35 -0400

    edits solution

--- AppBrew_WebDev/Javascript/CodeSnippets/bmiCalculator.js
+++ AppBrew_WebDev/Javascript/CodeSnippets/bmiCalculator.js
@@ -16,3 +16,23 @@ bmi should equal around 20 in this case.
 
 var bmi = bmiCalculator(65, 1.8);
 console.log(bmi)
+
+// Conditional BMI (IF/Else)
+
+function bmiCalculator(weight, height){
+    var bmi = Math.round(weight / (Math.power(height, 2)));
+    var interpretation;
+
+    if(bmi < 18.5){
+       interpretation = alert("Your BMI is " + bmi + ", so you are underweight.");
+    }
+    if(bmi >= 18.5 && bmi < 24.9 ){
+       interpretation = alert("Your BMI is " + bmi + ", so you have a normal weight.");
+    }
+    if(bmi > 24.9){
+       interpretation = alert("Your BMI is "  + bmi + ", so you are overweight.")
+    }
+
+    return interpretation;
+
+}
