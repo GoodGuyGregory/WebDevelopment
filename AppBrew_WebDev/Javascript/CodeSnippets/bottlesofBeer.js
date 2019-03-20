a8ae4cb30fe52b48103a40733337694805bff164
GoodGuyGregory <greg.witt625@gmail.com>
2019-03-20 10:29:10 -0400

    adds drunk text in browser

--- AppBrew_WebDev/Javascript/CodeSnippets/bottlesofBeer.js
+++ AppBrew_WebDev/Javascript/CodeSnippets/bottlesofBeer.js
@@ -3,18 +3,19 @@
 // to the bottles beers on the wall song
 
 function beer(){
-  var numberOfBottles = 99
-while (numberOfBottles >= 0) {
-     var bottleWord = "bottle";
-     if (numberOfBottles === 1) {
-         bottleWord = "bottles";
+  var numberOfBottles = 99;
+  while (numberOfBottles >= 0) {
+       var bottleWord = "bottle";
+       if (numberOfBottles === 1) {
+           bottleWord = "bottles";
+       }
+       console.log(numberOfBottles + " " + bottleWord + " of beer on the wall");
+       console.log(numberOfBottles + " " + bottleWord + " of beer,");
+       console.log("Take one down, pass it around,");
+        numberOfBottles--;
+       console.log(numberOfBottles + " " + bottleWord + " of beer on the wall.");
      }
-     console.log(numberOfBottles + " " + bottleWord + " of beer on the wall");
-     console.log(numberOfBottles + " " + bottleWord + " of beer,");
-     console.log("Take one down, pass it around,");
-      numberOfBottles--;
-     console.log(numberOfBottles + " " + bottleWord + " of beer on the wall.");
-   }
    console.log("No More bottes of Beer on the wall!");
-   
+   alert("Getting Drunk with a For Loop is easier");
+
 }
