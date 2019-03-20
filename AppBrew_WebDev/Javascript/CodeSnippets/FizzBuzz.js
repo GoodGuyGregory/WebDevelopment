2c7ec6e8f3f86a6d8579b5640d5d88bc9e8f5174
GoodGuyGregory <greg.witt625@gmail.com>
2019-03-20 09:31:39 -0400

    completes challenge

--- AppBrew_WebDev/Javascript/CodeSnippets/FizzBuzz.js
+++ AppBrew_WebDev/Javascript/CodeSnippets/FizzBuzz.js
@@ -5,15 +5,15 @@ var count = 1;
 
 function fizzBuzz() {
   count += 1;
-  if (count % 3 == 0){
+  if (count % 3 == 0 && count % 5 == 0) {
+    console.log("FizzBuzz");
+  }
+  if (count % 3 == 0) {
       output.push("Fizz");
   } else if (count % 5 == 0){
       output.push("Buzz");
-  } else if (count % 3 == 0 && count % 5 == 0) {
-    console.log("FizzBuzz");
   } else {
     output.push(count);
   }
     console.log(output);
-
-}
+}//fizzBuzz
