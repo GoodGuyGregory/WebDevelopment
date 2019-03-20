0c445f3b11ab1c13897f4c8e2d21aa5ee077070e
GoodGuyGregory <greg.witt625@gmail.com>
2019-03-20 09:34:24 -0400

    adds while loop

--- AppBrew_WebDev/Javascript/CodeSnippets/FizzBuzz.js
+++ AppBrew_WebDev/Javascript/CodeSnippets/FizzBuzz.js
@@ -4,16 +4,20 @@ var output = [];
 var count = 1;
 
 function fizzBuzz() {
-  count += 1;
-  if (count % 3 == 0 && count % 5 == 0) {
-    console.log("FizzBuzz");
-  }
-  if (count % 3 == 0) {
-      output.push("Fizz");
-  } else if (count % 5 == 0){
-      output.push("Buzz");
-  } else {
-    output.push(count);
-  }
-    console.log(output);
+
+while(count <= 100){
+    count += 1;
+    if (count % 3 == 0 && count % 5 == 0) {
+      console.log("FizzBuzz");
+    }
+    if (count % 3 == 0) {
+        output.push("Fizz");
+    } else if (count % 5 == 0){
+        output.push("Buzz");
+    } else {
+      output.push(count);
+    }
+      console.log(output);
+      count++;
+  }//while
 }//fizzBuzz
