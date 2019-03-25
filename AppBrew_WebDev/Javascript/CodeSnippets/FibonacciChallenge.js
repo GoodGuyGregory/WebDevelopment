3d28f097c641accdfab6242ca5c32bb2c54c90fe
GoodGuyGregory <greg.witt625@gmail.com>
2019-03-25 09:43:56 -0400

    adds code to the appbrew Project folder

--- AppBrew_WebDev/Javascript/CodeSnippets/FibonacciChallenge.js
+++ AppBrew_WebDev/Javascript/CodeSnippets/FibonacciChallenge.js
@@ -4,13 +4,19 @@
 
 function fibonacciGenerator (n) {
     //TODO
-    for (var nsum = 0; nsum <= n ; nsum++) {
-      // Summation Calculation:
-      var result = (nsum += nsum);
-      fibonacciNums.push(result);
-    }
-    //Return an array of fibonacci numbers.
-
-    console.log(fibonacciNums);
+    // create the array
+    var fib = [];
+      // Loops through the array based on the input
+     for (var i = 0; i < n; i++) {
+        // first series solution
+         fib.push(i);
+         // 1 + K case -> infinity
+         if(i > 1) {
+              // entry = previous entry + second previous entry
+              fib[i] = fib[i-1] + fib[i-2];
+         }
+     }
+     // returns the array with the correct numbers
+     return fib;
 
 }
