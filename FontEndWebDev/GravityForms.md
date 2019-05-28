60edfbcf04279503b608219ef678c96753f873ef
GoodGuyGregory <greg.witt625@gmail.com>
2019-05-28 15:15:52 -0400

    local changes

---
+++ FontEndWebDev/GravityForms.md
@@ -0,0 +1,4 @@
+# Gravity Forms:  
+-----------------  
+
+### Custom Stylings:  
--- JavaScript/Js and the DOM/app.js
+++ JavaScript/Js and the DOM/app.js
@@ -1,8 +1,43 @@
-const input = document.querySelector('input');
-const p = document.querySelector('p.description');
-const button = document.querySelector('button');
+const toggleList = document.getElementById('toggleList');
+const listDiv = document.querySelector('.list');
+const descriptioninput = document.querySelector('input.description');
+const descriptionp = document.querySelector('p.description');
+const descriptionButton = document.querySelector('descriptionButton');
+const addItemInput = document.querySelector('input.addItemInput');
+const addItemButton = document.querySelector('input.addItemButton');
+const removeItemButton = document.querySelector('input.removeItemButton');
 
-button.addEventListener('click', () => {
-  p.innerHTML = input.value + ':';
+// adds addEventListener
+toggleList.addEventListener('click', () => {
+  if (listDiv.style.display == 'none') {
+    toggleList.textContent = 'Hide list';
+    listDiv.style.display = 'block';
+  } else {
+    toggleList.textContent = 'Show list';
+    listDiv.style.display = 'block';
+  }
+  listDiv.style.display = 'none';
 });
 
+descriptionbutton.addEventListener('click', () => {
+  descriptionp.innerHTEML = descriptioninput.value + ':';
+  descriptionp.value = '';
+});
+
+//appendChild method
+// Node.addChild(<ChildElement)
+addItemButton.addEventListener('click', () => {
+  let ul = document.getElementByTageName('ul')[0];
+  let li = document.createElement('li');
+  li.textContent = addItemInput.value;
+  //appends child li
+  ul.appendChild(li);
+  addItemInput.value = '';
+});
+
+// Remove Child Selector
+removeItemButton.addEventListener('click', () => {
+  let ul = document.getElementByTageName('ul')[0];
+  let li = document.querySelector('li:last-child');
+  li.removeChild('li');
+});
