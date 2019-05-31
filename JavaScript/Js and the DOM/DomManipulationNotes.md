e7d8c2b62e405e248db4d7c53bec3b3d698f751d
GoodGuyGregory <greg.witt625@gmail.com>
2019-05-31 14:47:17 -0400

    adds project changes

--- JavaScript/Js and the DOM/DomManipulationNotes.md
+++ JavaScript/Js and the DOM/DomManipulationNotes.md
@@ -8,6 +8,8 @@
 
 Basic events check [MDN](https://developer.mozilla.org/en-US/docs/Web/Events)
 
+event handlers and listeners can peculate up the DOM back to the document this is called ***bubbling*** and allows us to manipulate ancestor elements.This ***bubbling*** technique will allow users to keep a DRY programming and coding style to allow less handlers on child Elements.
+
 **Laptop/Desktops**  
 
   * click
@@ -24,4 +26,30 @@ Basic events check [MDN](https://developer.mozilla.org/en-US/docs/Web/Events)
   * touchmove  
   * touchend  
 
-  
+## DOM Traversal Concepts  
+
+Referencing elements near each other on the DOM tree.  
+
+the code snippet below will remove a paragraph element from the DOM's parent element of that paragraph.  
+
+the problem here is that it would be easier to target the specific parent by using the `parent.removeChild(paragraph);`
+
+**Basics**  
+
+```JavaScript
+  let paragraph = document.getElementById('myParagraph');
+  //removes the child element from the Parent
+  parent.removeChild(paragraph);
+
+```  
+**parentNode**  
+
+```JavaScript
+  let paragraph = document.getElementById('myParagraph');
+  //lets a variable hold the unknown parent value
+  let parent = paragraph.parentNode;
+
+  //removes the child element from the known/unknown parent element
+  parent.removeChild(paragraph);
+
+```
--- JavaScript/Js and the DOM/ExperimentalProject/app.js
+++ JavaScript/Js and the DOM/ExperimentalProject/app.js
@@ -1,4 +1,6 @@
 const toggleList = document.getElementById('toggleList');
+
+// Main Parent element for all of our previously coded event handlers
 const listDiv = document.querySelector('.list');
 const descriptioninput = document.querySelector('input.description');
 const descriptionp = document.querySelector('p.description');
@@ -8,16 +10,35 @@ const addItemButton = document.querySelector('input.addItemButton');
 const removeItemButton = document.querySelector('input.removeItemButton');
 const listItems = document.getElementsByTagName('li');
 
-for (let i = 0; i < listItems.length; i += 1) {
-    // adds an Event Listener on the listitems on the page for mouseover event
-  listItems[i].addEventListener('mouseover', () => {
-    listItems[i].textContent = listItems[i].textContent.toUpperCase();
+event.target.tagName
+
+// ListDiv is the highest Ancestor Element on this Page
+  // adds an Event Listener on the listitems on the page for mouseover event
+  listDiv.addEventListener('click', (event) => {
+// tageName returns an element's tag in all caps
+    if (event.target.tagName === 'BUTTON') {
+      // sets the target to a variable
+      let li = event.target.parentNode;
+      // moves up a tag element and gets the parent name/attributes for
+      // Targeting that specific node on the DOM
+      let ul = li.parentNode;
+      // Removes that child based on the specific parent node
+      ul.removeChild(li);
+    }
   });
   // adds an Event Listener for when the mouse leaves
-  listItems[0].addEventListener('mouseout', () => {
-    listItems[0].textContent = listItems[0].textContent.toUpperCase();
-    });
-}
+  // listDiv.addEventListener('mouseout', (event) => {
+  //   if (event.target.tagName === 'LI') {
+  //   event.target.textContent = event.target.textContent.toUpperCase();
+  //   }
+  // });
+
+
+
+//console logs events that are clicked
+// document.addEventListener('click', (event) => {
+//   console.log(event.target);
+// });
 
 // adds addEventListener
 toggleList.addEventListener('click', () => {
